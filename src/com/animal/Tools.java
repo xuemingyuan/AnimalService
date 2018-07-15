ae12733c2dd14dd8237f81dd7987e35c3fc6e285
jlli <jlli@MPI-JLLI-PC.medicilon.com.cn>
2018-07-15 14:29:47 +0800

    review

--- src/com/animal/Tools.java
+++ src/com/animal/Tools.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.apache.poi.hwpf.HWPFDocument;
@@ -25,15 +26,26 @@ public class Tools {
 		Logger.getRootLogger().setLevel(Level.ALL);
 	}
 
+	public static String getProjectPath() throws UnsupportedEncodingException {
+
+		java.net.URL url = Tools.class.getProtectionDomain().getCodeSource().getLocation();
+		String filePath = java.net.URLDecoder.decode(url.getPath(), "utf-8");
+		if (filePath.endsWith(".jar"))
+			filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+		java.io.File file = new java.io.File(filePath);
+		filePath = file.getAbsolutePath();
+		return filePath;
+	}
+
 	public static void main(String[] args) throws Exception {
 
 		logger.debug(" Begin. please wait one minute.");
 		// D:/Java/ Monkey
-		String mainPath = args[0];
-		String kind = args[1];
-		 mainPath = "D:/Java/";
+		logger.debug(getProjectPath());
+		String mainPath = getProjectPath();
+		String kind = args[0];
 		 kind = "Monkey";
-		String path = mainPath + "xls/test.xls";
+		String path = mainPath + "/xls/test.xls";
 		List<String> xlsList = XlsUtils.readXls(path);
 		Tools f = new Tools();
 		f.testWrite(xlsList, mainPath, kind);
@@ -42,9 +54,9 @@ public class Tools {
 	}
 
 	public void testWrite(List<String> xlsList, String mainPath, String kind) throws Exception {
-		String tempDir = mainPath + "template";
+		String tempDir = mainPath + "/template";
 
-		clearMainPathDoc(mainPath + "doc");
+		clearMainPathDoc(mainPath + "/doc");
 
 		File directory = new File(tempDir);
 		for (File templateFile : directory.listFiles()) {
@@ -62,7 +74,7 @@ public class Tools {
 					range.replaceText("${tattooNo}", arr[1]);
 					range.replaceText("${Sex}", arr[2]);
 					range.replaceText("${Species}", kind);
-					File dirPa = new File(mainPath + "doc/" + kind + "/" + allType);
+					File dirPa = new File(mainPath + "/doc/" + kind + "/" + allType);
 					dirPa.mkdirs();
 
 					File file = new File(dirPa.getPath() + "/" + arr[0] + "_" + templateFile.getName());
@@ -76,14 +88,11 @@ public class Tools {
 	private void clearMainPathDoc(String path) {
 
 		File file = new File(path);
-		if(file.isDirectory())
-		{
-			for(File f:file.listFiles())
-			{
+		if (file.isDirectory()) {
+			for (File f : file.listFiles()) {
 				clearMainPathDoc(f.getPath());
 			}
-		}
-		else
+		} else
 			file.delete();
 
 	}
