39a09084b5fc0289adc0f3da0678e136c3d71eb6
Administrator <Administrator@10.101.20.58>
2018-07-15 14:50:07 +0800

    add pom.xml

---
+++ WebContent/WEB-INF/classes/Monkey.bat
@@ -0,0 +1,4 @@
+echo 'test begin'
+java -cp lib/commons-codec-1.10.jar;lib/commons-collections4-4.1.jar;lib/commons-logging-1.2.jar;lib/curvesapi-1.04.jar;lib/dom4j-1.6.1.jar;lib/junit-4.12.jar;lib/log4j-1.2.17.jar;lib/poi-3.17.jar;lib/poi-examples-3.17.jar;lib/poi-excelant-3.17.jar;lib/poi-ooxml-3.17.jar;lib/poi-ooxml-schemas-3.17.jar;lib/poi-scratchpad-3.17.jar;lib/xmlbeans-2.6.0.jar;template.jar com.animal.Tools Monkey
+echo 'test end'
+pause
